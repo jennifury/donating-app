34a3d32ce33b206ae94fc3b99df51477c36b6620
Jennifer Yang <jenniferyang@Jennifers-MacBook-Pro.local>
2019-07-30 15:13:53 -0700

    updated

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import webapp2
 import jinja2
 import os
+from google.appengine.ext import ndb
 
 don_app_env = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -16,6 +17,11 @@ class HomeHandler(webapp2.RequestHandler):
     def get(self):
         donation_template = don_app_env.get_template('templates/homepage.html')
         self.response.write(donation_template.render())
+#database
+class NewPost(ndb.Model):
+    name = ndb.StringProperty(required=True)
+    item = ndb.StringProperty(required=True)
+    paragraph = ndb.StringProperty(required=True)
 
 class NewPostHandler(webapp2.RequestHandler):
     def get(self):
@@ -23,12 +29,15 @@ class NewPostHandler(webapp2.RequestHandler):
         self.response.write(post_template.render())
 
     def post(self):
-        post_template= don_app_env.get_template('templates/post_upload.html')
-        poster_name = self.request.get('name')
+        poster_name = self.request.get('username')
         post_item = self.request.get('item')
-        post_pic = self.request.get('pic')
+        post_pics = self.request.get('pics')
         post_desc = self.request.get('paragraph')
-        self.response.write(post_template.render())
+
+        new_name = NewPost(name=poster_name, item = post_item, paragraph = post_desc)
+        new_name.put()
+        self.redirect("/thankyou")
+
 
 class ItemHandler(webapp2.RequestHandler):
     def get(self):
@@ -40,11 +49,16 @@ class DonationDisplayHandler(webapp2.RequestHandler):
         donation_display_template= don_app_env.get_template('templates/donation_display.html')
         self.response.write(donation_display_template.render())
 
+class ThanksHandler(webapp2.RequestHandler):
+    def get(self):
+        thanks_template= don_app_env.get_template('templates/thank_you.html')
+        self.response.write(thanks_template.render())
 
 app = webapp2.WSGIApplication([
     ('/', HomeHandler),
     ('/about_us', AboutUsHandler),
     ('/donation_display', DonationDisplayHandler),
     ('/newdonation', NewPostHandler),
-    ('/item', ItemHandler)
+    ('/item', ItemHandler),
+    ('/thankyou', ThanksHandler)
 ], debug=True)
