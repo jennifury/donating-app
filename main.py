ee27fdabd536d11b3c076b4dfd54ffadbe162502
jargar-binks <53350264+jargar-binks@users.noreply.github.com>
2019-07-30 16:48:35 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import webapp2
 import jinja2
 import os
 from google.appengine.ext import ndb
+from google.appengine.ext import db
 
 don_app_env = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -19,6 +20,7 @@ class HomeHandler(webapp2.RequestHandler):
         self.response.write(donation_template.render())
 
 class NewPost(ndb.Model):
+    image = ndb.BlobProperty()
     name = ndb.StringProperty(required=True)
     item = ndb.StringProperty(required=True)
     paragraph = ndb.StringProperty(required=True)
@@ -29,12 +31,15 @@ class NewPostHandler(webapp2.RequestHandler):
         self.response.write(post_template.render())
 
     def post(self):
-        poster_name = self.request.get('name')
+        poster_name = self.request.get('username')
         post_item = self.request.get('item')
         post_pics = self.request.get('pics')
+        # file_upload = self.request.POST.get("pics", None)
         post_desc = self.request.get('paragraph')
 
-        new_name = NewPost(name=poster_name, item = post_item, paragraph = post_desc)
+        # new_name = NewPost(name=poster_name, item = post_item, paragraph = post_desc, image= file_upload.file.read())
+        new_name = NewPost(name=poster_name, item = post_item, paragraph = post_desc, image= db.Blob(str(post_pics))  )
+
         new_name.put()
         self.redirect("/thankyou")
 
@@ -46,8 +51,31 @@ class ItemHandler(webapp2.RequestHandler):
 
 class DonationDisplayHandler(webapp2.RequestHandler):
     def get(self):
+       
+        posts = NewPost.query().fetch()
+        image_dict = {
+        "num_of_items": len(posts)
+        }
+
         donation_display_template= don_app_env.get_template('templates/donation_display.html')
-        self.response.write(donation_display_template.render())
+        self.response.write(donation_display_template.render(image_dict))
+
+class ImageHandler(webapp2.RequestHandler):
+    def get(self):
+        posts = NewPost.query().fetch()
+
+        image_dict = {
+        "num_of_items": len(posts)
+        }
+        index = self.request.get('index')
+        index = int(index)
+        posts[index].image
+        if posts[index].image:
+          self.response.headers['Content-Type'] = "image/jpg"
+          self.response.out.write(posts[index].image)
+        else:
+          self.redirect('static/noimage.svg')
+
 
 class ThanksHandler(webapp2.RequestHandler):
     def get(self):
@@ -57,8 +85,9 @@ class ThanksHandler(webapp2.RequestHandler):
 app = webapp2.WSGIApplication([
     ('/', HomeHandler),
     ('/about_us', AboutUsHandler),
-    ('/donation_display', DonationDisplayHandler),
+    ('/displaydonations', DonationDisplayHandler),
     ('/newdonation', NewPostHandler),
     ('/item', ItemHandler),
-    ('/thankyou', ThanksHandler)
+    ('/thankyou', ThanksHandler),
+    ('/imageit', ImageHandler)
 ], debug=True)
